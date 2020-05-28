e68866e37bd906aa6dcd7380a6fc22cfc15efeac
Mieszko <mieszko>
2020-05-27 23:00:42 -0700

    ARC4 lab
    change reset spec to sync

---
+++ task1/init.sv
@@ -0,0 +1,7 @@
+module init(input logic clk, input logic rst_n,
+            input logic en, output logic rdy,
+            output logic [7:0] addr, output logic [7:0] wrdata, output logic wren);
+
+// your code here
+
+endmodule: init
